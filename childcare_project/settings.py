e293028825fe014eb54860331d7e6ffdab505018
Lakshya <lakshyadbasur31@gmail.com>
2026-06-12 19:09:57 +0530

    Allow Vercel domains in ALLOWED_HOSTS

--- childcare_project/settings.py
+++ childcare_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hy7r&&e#36m%ya^ll3^$e*xx&re-9w8i6)+ie(xpj+_f%iw0^_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
