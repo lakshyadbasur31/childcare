fecc2cb8ead8ad140f79677914e9892f1c712dc1
Lakshya <lakshyadbasur31@gmail.com>
2026-06-12 20:02:46 +0530

    Revert "Add Vercel Postgres database configuration"
    This reverts commit 414195ddb98893ee6a99085495baa86978c4f45e.

--- childcare_project/settings.py
+++ childcare_project/settings.py
@@ -73,23 +73,12 @@ WSGI_APPLICATION = 'childcare_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import os
-import dj_database_url
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# If Vercel Postgres is connected, it will provide POSTGRES_URL
-if os.environ.get('POSTGRES_URL'):
-    DATABASES['default'] = dj_database_url.config(
-        default=os.environ.get('POSTGRES_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
 
 
 # Password validation
