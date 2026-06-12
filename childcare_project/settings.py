71a83bdb24453f9a3bcd225dbf9d8f72369de40e
Lakshya <lakshyadbasur31@gmail.com>
2026-06-12 21:24:20 +0530

    Resolve requirements.txt conflict and use Neon DATABASE_URL

--- childcare_project/settings.py
+++ childcare_project/settings.py
@@ -77,12 +77,23 @@ WSGI_APPLICATION = 'childcare_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# If Vercel Postgres (Neon) is connected, it will provide DATABASE_URL
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 
 
 # Password validation
